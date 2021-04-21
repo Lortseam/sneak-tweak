c7f66836be4eede75f799d693ba3c86e0362772e
Lortseam <lortseam@gmail.com>
2021-04-21 17:16:48 +0200

    Update dependencies

--- src/main/java/me/lortseam/sneaktweak/SneakTweak.java
+++ src/main/java/me/lortseam/sneaktweak/SneakTweak.java
@@ -1,6 +1,5 @@
 package me.lortseam.sneaktweak;
 
-import lombok.Getter;
 import me.lortseam.completeconfig.data.Config;
 import me.lortseam.sneaktweak.config.Settings;
 import net.fabricmc.api.ClientModInitializer;
@@ -8,12 +7,10 @@ import net.fabricmc.api.ClientModInitializer;
 public class SneakTweak implements ClientModInitializer {
 
     public static final String MOD_ID = "sneaktweak";
-    @Getter
-    private static Config config;
 
     @Override
     public void onInitializeClient() {
-        config = Config.builder(MOD_ID)
+        Config.builder(MOD_ID)
                 .add(new Settings())
                 .build();
     }
