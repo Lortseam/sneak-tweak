ecf65ad67d0fd16ab681c8c467844c9daa217bbd
Lortseam <lortseam@gmail.com>
2022-05-21 16:11:30 +0200

    Update to 1.19-pre.1

--- src/main/java/me/lortseam/sneaktweak/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/me/lortseam/sneaktweak/mixin/ClientPlayerEntityMixin.java
@@ -8,13 +8,15 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityPose;
+import net.minecraft.network.encryption.PlayerPublicKey;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
 
-    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
-        super(world, profile);
+    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile, @Nullable PlayerPublicKey publicKey) {
+        super(world, profile, publicKey);
     }
 
     @Override
